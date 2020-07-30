801d8e14483f495d05a9dbf076ec5da53c3cba03
VjacheslavZ <vjacheslav.zemlianiy@fluvius.co>
2020-07-30 12:42:42 +0300

    svg

--- test/src/Screens/Weather/index.js
+++ test/src/Screens/Weather/index.js
@@ -10,14 +10,17 @@ import {
 export const WeatherScreen = (props) => {
   const { data } = props.route.params;
   const { temperature, weather_icons = [], wind_speed, precip } = data.current;
-
+  console.log('weather_icons', weather_icons)
   return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Temperature: {temperature}</Text>
         <Text>Wind Speed: {wind_speed}</Text>
         <Text>Precip: {precip}</Text>
 
-        {/*{weather_icons.map(uri => <SvgUri key={uri} width='100%' height="600" uri={uri}/>)}*/}
+        {weather_icons.map(uri =>         <Image
+          sourcr={{uri, width: 60, height: 50}}
+          key={uri}
+        />)}
       </View>
     )
 };
